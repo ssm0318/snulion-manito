bc25f05e5c1d94afe50a21203d9b81047a4cf9ac
ssm0318 <kleenex0318@gmail.com>
2019-03-16 01:11:20 +0900

    tabs

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'posts#index'
-  get '/posts/new' => 'posts#new'
+  get '/' => 'posts#index', as: :posts
+  get '/posts/new' => 'posts#new', as: :new_post
   post '/posts' => 'posts#create'
   delete '/posts/:id' => 'posts#destroy'
   post '/posts/:id/comments' => 'posts#create_comment'
