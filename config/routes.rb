fe036296b6ef7166781650bb31db9e7b6312bcc4
ssm0318 <kleenex0318@gmail.com>
2019-03-15 10:53:09 +0900

    devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'posts#index'
   get '/posts/new' => 'posts#new'
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   post '/posts/:id/comments' => 'posts#create_comment'
   delete '/posts/:id/comments/:comment_id' => 'posts#destroy_comment'
   post '/post_commments/:id' => 'posts#create_reply'
+
+  # https://github.com/plataformatec/devise
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126160049) do
+ActiveRecord::Schema.define(version: 20190315012044) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20181126160049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["comment_id"], name: "index_replies_on_comment_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
